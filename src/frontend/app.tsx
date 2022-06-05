accfe89bd8152f35ee07d4a9cfc9d2381a6e82a9
Michael <michaelsalow@web.de>
2022-06-05 17:33:41 +0200

    feat: use first ipc very crudely

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,8 +1,26 @@
+import { IpcMainInvokeEvent } from 'electron';
+import { useState } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { DayDTO } from '../backend/api/updateDay';
 
+type electronAPI = {
+    getAllDays:(month: number) =>Promise<DayDTO[]>
+}
+declare global {
+    interface Window {
+        myAPI: electronAPI;
+    }
+}
 const root = ReactDOM.createRoot(document.getElementById('app'));
 function App() {
-    return (<div><h2>Hello from React!</h2><p>Current month is June</p></div>);
+    const [days, setDays ] = useState([]);
+    window.myAPI.getAllDays(3).then(daysFromAPI => {
+        setDays(daysFromAPI);
+    });
+    return (<div>
+        <h2>Hello from React!</h2>
+        <p>Current month is June</p>
+        <ul>{days.map((day: DayDTO) => { return (<li>{day.year}</li>)})}</ul></div>);
 }
 
 root.render(<App></App>)
