febea56e9e15e9afe5155445a9a19095f6f202e3
Michael <michaelsalow@web.de>
2022-06-06 10:36:47 +0200

    feat: first crude implementation

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,26 +1,37 @@
-import { IpcMainInvokeEvent } from 'electron';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { electronAPI } from '../preload';
 import { DayDTO } from '../backend/api/updateDay';
 
-type electronAPI = {
-    getAllDays:(month: number) =>Promise<DayDTO[]>
-}
 declare global {
     interface Window {
         myAPI: electronAPI;
     }
 }
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const containerElement = document.getElementById('app')
+if (!containerElement) {
+    throw new Error('did not find root container with id "app"');
+}
+const root = ReactDOM.createRoot(containerElement);
 function App() {
-    const [days, setDays ] = useState([]);
-    window.myAPI.getAllDays(3).then(daysFromAPI => {
+    const [days, setDays ] = useState([] as DayDTO[]);
+    const [month, setMonth] = useState(6);
+    const updateDays = async(month: number) => {
+        console.log('call to myApi with month', month);
+        const daysFromAPI = await window.myAPI.getAllDays(2022, month);
+        console.log('received days %o', daysFromAPI);
         setDays(daysFromAPI);
-    });
+    }
+    useEffect(() => {
+        updateDays(month);
+    }, [month])
     return (<div>
         <h2>Hello from React!</h2>
         <p>Current month is June</p>
-        <ul>{days.map((day: DayDTO) => { return (<li>{day.year}</li>)})}</ul></div>);
+        <select onChange={(event) => setMonth(parseInt(event.target.value,10))}>
+            {[1,2,3,4,5,6,7,8,9,10,11,12].map(monthNumber => (<option>{monthNumber}</option>))}
+        </select>
+        <ul>{days.map((day: DayDTO) => {  return (<li style={day.sweetConsumption ? {backgroundColor: 'red'} : {}}>{day.year} {day.month} {day.day} {day.sweetConsumption}</li>)})}</ul></div>);
 }
 
 root.render(<App></App>)
